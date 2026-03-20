ea42da35c04f53fe580d3050b2c156e6c4022a95
Admin <erarvinder2012@gmail.com>
2026-03-20 16:13:45 +0200

    super

--- scpminmemory/src/main/java/com/scpm/inmemory/scpminmemory/userService/config/filterchain/SecurityConfigurations.java
+++ scpminmemory/src/main/java/com/scpm/inmemory/scpminmemory/userService/config/filterchain/SecurityConfigurations.java
@@ -200,7 +200,7 @@ public class SecurityConfigurations {
                                 .requestMatchers("/api/user/getApplicetRole/{applicentrole}").permitAll()
                                 .requestMatchers("/api/user/confirmTeacherRole").permitAll()
                                 .requestMatchers("/api/user/getAllApplicets").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
